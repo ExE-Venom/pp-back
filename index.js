d0ac147336dc60064c7bb276528650936b27873e
ExE-Venom <vrenom28@gmail.com>
2025-05-18 19:40:10 +0530

    gg

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Welcome to the PhonePe Payment API");
-}
+});
 
 app.post("/pay", async (req, res) => {
   const { amount } = req.body;
