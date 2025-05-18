fe92479e6acb8a4aa5b164044ca95ed1a800806b
ExE-Venom <vrenom28@gmail.com>
2025-05-18 12:22:59 +0530

    f

--- index.js
+++ index.js
@@ -1,54 +1,40 @@
-// importing modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
 const sha256 = require("sha256");
 const uniqid = require("uniqid");
+require("dotenv").config();
 
-// creating express application
 const app = express();
-
-// UAT environment
-const MERCHANT_ID = "PGTESTPAYUAT";
-const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
+const MERCHANT_ID = process.env.MERCHANT_ID;
 const SALT_INDEX = 1;
-const SALT_KEY = "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399";
-const APP_BE_URL = "http://localhost:3002"; // our application
+const SALT_KEY = process.env.SALT_KEY;
+const PHONE_PE_HOST_URL = process.env.PHONE_PE_HOST_URL;
+const APP_BE_URL = process.env.APP_BE_URL; // our application
+const APP_FE_URL = process.env.APP_FE_URL; // frontend application
 
-// setting up middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Defining a test route
 app.get("/", (req, res) => {
   res.send("PhonePe Integration APIs!");
 });
 
-// endpoint to initiate a payment
 app.get("/pay", async function (req, res, next) {
-  // Initiate a payment
-
-  // Transaction amount
   const amount = +req.query.amount;
-
-  // User ID is the ID of the user present in our application DB
-  let userId = "MUID123";
-
-  // Generate a unique merchant transaction ID for each transaction
+  let userId = uniqid();
+  if (!amount || isNaN(amount) || amount <= 0) {
+    return res.status(400).send({ error: "Invalid amount" });
+  }
   let merchantTransactionId = uniqid();
 
-  // redirect url => phonePe will redirect the user to this url once payment is completed. It will be a GET request, since redirectMode is "REDIRECT"
   let normalPayLoad = {
-    merchantId: MERCHANT_ID, //* PHONEPE_MERCHANT_ID . Unique for each account (private)
+    merchantId: MERCHANT_ID,
     merchantTransactionId: merchantTransactionId,
     merchantUserId: userId,
-    amount: amount * 100, // converting to paise
+    amount: amount * 100,
     redirectUrl: `${APP_BE_URL}/payment/validate/${merchantTransactionId}`,
     redirectMode: "REDIRECT",
     mobileNumber: "9999999999",
@@ -57,17 +43,15 @@ app.get("/pay", async function (req, res, next) {
     },
   };
 
-  // make base64 encoded payload
   let bufferObj = Buffer.from(JSON.stringify(normalPayLoad), "utf8");
   let base64EncodedPayload = bufferObj.toString("base64");
 
-  // X-VERIFY => SHA256(base64EncodedPayload + "/pg/v1/pay" + SALT_KEY) + ### + SALT_INDEX
   let string = base64EncodedPayload + "/pg/v1/pay" + SALT_KEY;
   let sha256_val = sha256(string);
   let xVerifyChecksum = sha256_val + "###" + SALT_INDEX;
 
-  axios
-    .post(
+  try {
+    const response = await axios.post(
       `${PHONE_PE_HOST_URL}/pg/v1/pay`,
       {
         request: base64EncodedPayload,
@@ -79,59 +63,44 @@ app.get("/pay", async function (req, res, next) {
           accept: "application/json",
         },
       }
-    )
-    .then(function (response) {
-      console.log("response->", JSON.stringify(response.data));
-      res.redirect(response.data.data.instrumentResponse.redirectInfo.url);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
-});
-
-// endpoint to check the status of payment
-app.get("/payment/validate/:merchantTransactionId", async function (req, res) {
-  const { merchantTransactionId } = req.params;
-  // check the status of the payment using merchantTransactionId
-  if (merchantTransactionId) {
-    let statusUrl =
-      `${PHONE_PE_HOST_URL}/pg/v1/status/${MERCHANT_ID}/` +
-      merchantTransactionId;
-
-    // generate X-VERIFY
-    let string =
-      `/pg/v1/status/${MERCHANT_ID}/` + merchantTransactionId + SALT_KEY;
-    let sha256_val = sha256(string);
-    let xVerifyChecksum = sha256_val + "###" + SALT_INDEX;
-
-    axios
-      .get(statusUrl, {
-        headers: {
-          "Content-Type": "application/json",
-          "X-VERIFY": xVerifyChecksum,
-          "X-MERCHANT-ID": merchantTransactionId,
-          accept: "application/json",
-        },
-      })
-      .then(function (response) {
-        console.log("response->", response.data);
-        if (response.data && response.data.code === "PAYMENT_SUCCESS") {
-          // redirect to FE payment success status page
-          res.send(response.data);
-        } else {
-          // redirect to FE payment failure / pending status page
-        }
-      })
-      .catch(function (error) {
-        // redirect to FE payment failure / pending status page
-        res.send(error);
-      });
-  } else {
-    res.send("Sorry!! Error");
+    );
+    res.redirect(response.data.data.instrumentResponse.redirectInfo.url);
+  } catch (error) {
+    res.status(500).send({ error: "Failed to initiate payment", details: error.response?.data || error.message });
   }
 });
 
-// Starting the server
+app.get("/payment/validate/:merchantTransactionId", async function (req, res) {
+  const { merchantTransactionId } = req.params;
+  if (merchantTransactionId) {
+    let statusUrl = `${PHONE_PE_HOST_URL}/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}`;
+
+    let string = `/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}` + SALT_KEY;
+    let sha256_val = sha256(string);
+    let xVerifyChecksum = sha256_val + "###" + SALT_INDEX;
+
+    try {
+      const response = await axios.get(statusUrl, {
+        headers: {
+          "Content-Type": "application/json",
+          "X-VERIFY": xVerifyChecksum,
+          "X-MERCHANT-ID": MERCHANT_ID,
+          accept: "application/json",
+        },
+      });
+      if (response.data && response.data.code === "PAYMENT_SUCCESS") {
+        res.redirect(`${APP_FE_URL}/payment/status/PAYMENT_SUCCESS`);
+      } else {
+        res.redirect(`${APP_FE_URL}/payment/status/${response.data.code}`);
+      }
+    } catch (error) {
+      res.redirect(`${APP_FE_URL}/payment/status/ERROR`);
+    }
+  } else {
+    res.redirect(`${APP_FE_URL}/payment/status/ERROR`);
+  }
+});
+
 const port = 3002;
 app.listen(port, () => {
   console.log(`PhonePe application listening on port ${port}`);
