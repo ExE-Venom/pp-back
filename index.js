2b7addf26ac4d48eb70a3b993335c90ee5df56f2
ExE-Venom <vrenom28@gmail.com>
2025-05-18 14:56:53 +0530

    gg

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.APP_FE_URL || "https://pay.rexzbot.xyz",
+    origin: "*",
     optionsSuccessStatus: 200,
   })
 );
